83c6e06d115ea4c1b6a244d10ad9451a20e29f05
Andrej Agarin <104525728+Andy-agandy1252@users.noreply.github.com>
2023-05-05 18:02:07 +0200

    Add files via upload

--- mysite/newsportal/urls.py
+++ mysite/newsportal/urls.py
@@ -1,12 +1,23 @@
 from django.urls import path
-
 from .import views
-
+from .views import crypto, crypto_detail
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('news/', views.news, name='news'),
+    path('article/<path:article_link>/', views.article_detail, name='article_detail'),
+    path('register/', views.register, name='register'),
+    path('crypto/', views.crypto, name='crypto'),
+    path('crypto/<id>/', views.crypto_detail, name='crypto_detail'),
+
+
+
+
+
+
+
+    # path('news/', views.news, name='news'),cd mysite
     # path('markets/', views.markets, name='markets'),
     # path('pro/', views.pro, name='pro'),
     # path('signin/', views.signin, name='signin'),
     # path('signup/', views.signup, name='signup'),
 ]
+
