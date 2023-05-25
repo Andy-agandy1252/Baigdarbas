a658b8c4ed8b3a812b80fcedb6ac5b025c7e1af7
Andrej Agarin <104525728+Andy-agandy1252@users.noreply.github.com>
2023-05-25 19:40:16 +0200

    Update urls.py
    add spase, pip

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pergale/', include('newsportal.urls')),
-
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', RedirectView.as_view(url='pergale/', permanent=True)),
+]+ (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
-
 ]
